098c3ecac3a59e4f298c997a1690ddf0b6ff1dde
Oliver <1571880470@qq.com>
2024-01-10 12:11:31 +0800

    fix: make async false by default

--- lua/autoime/config/init.lua
+++ lua/autoime/config/init.lua
@@ -2,7 +2,7 @@ local config = {
 	im_select_path = "im-select",
 	autoime_enabled = true,
 	default_im = "com.apple.keylayout.ABC",
-	async_if_able = true,
+	async_if_able = false,
 	_notice = false,
 }
 
